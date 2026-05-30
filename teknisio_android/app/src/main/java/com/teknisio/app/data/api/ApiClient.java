14da70870e115cbd923acde711eaae703ee0762b
IKIII0 <rifkialsauqy@students.usu.ac.id>
2026-05-30 18:46:16 +0700

    Memakai Database dan Backend online (Railway)

--- teknisio_android/app/src/main/java/com/teknisio/app/data/api/ApiClient.java
+++ teknisio_android/app/src/main/java/com/teknisio/app/data/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://10.0.2.2:8080/"; // 10.0.2.2 is localhost for Android Emulator
+    private static final String BASE_URL = "https://steadfast-liberation-production-e36b.up.railway.app/"; // 10.0.2.2 is localhost for Android Emulator
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
