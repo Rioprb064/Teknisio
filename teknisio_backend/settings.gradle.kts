a68ed99e3f38e3de6b27b88fa55a6effa361a62c
IKIII0 <rifkialsauqy@students.usu.ac.id>
2026-05-30 13:26:46 +0700

    fix: move pluginManagement before plugins

--- teknisio_backend/settings.gradle.kts
+++ teknisio_backend/settings.gradle.kts
@@ -1,13 +1,13 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 pluginManagement {
     repositories {
         maven { url = uri("https://repo.spring.io/release") }
         mavenCentral()
         gradlePluginPortal()
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 dependencyResolutionManagement {
